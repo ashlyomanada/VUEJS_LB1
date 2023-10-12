ea39c96f12b1fc6d5a9ffb1986c2e0d9bed3cb2b
ibonkarylle <144770046+ibonkarylle@users.noreply.github.com>
2023-10-12 12:38:54 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,23 @@
-<template></template>
+<template>
+  <nav></nav>
+  <div class="container">
+    <div class="form-box">
+      <form class="form">
+        <span class="title">Sign up</span>
+        <span class="subtitle">Create Your account here!</span>
+        <div class="form-container">
+          <input type="text" class="input" placeholder="Full Name" />
+          <input type="email" class="input" placeholder="Email" />
+          <input type="password" class="input" placeholder="Password" />
+        </div>
+        <button>Sign up</button>
+      </form>
+      <div class="form-section">
+        <p>Have an account? <a href="">Log in</a></p>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
